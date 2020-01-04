c988dde818100f23fa6818fa14d049d0b9b974e3
ibrokemypie <ibrokemypie@bastardi.net>
2020-01-04 21:45:48 +1100

    fix typescript setup

--- app/App.tsx
+++ app/App.tsx
@@ -1,15 +1,59 @@
-/**
- * @format
- */
+import React from 'react';
+import {FlatList, ActivityIndicator, Text, View} from 'react-native';
 
-import React, {Component} from 'react';
-import {Text, View} from 'react-native';
+export default class FetchExample extends React.Component<
+  {},
+  {
+    isLoading: boolean;
+    dataSource: ReadonlyArray<{
+      title: string;
+      releaseYear: number;
+      id: string;
+    }> | null;
+  }
+> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {isLoading: true, dataSource: null};
+  }
 
-export default class HelloWorldApp extends Component {
+  componentDidMount() {
+    return fetch('https://facebook.github.io/react-native/movies.json')
+      .then(response => response.json())
+      .then(responseJson => {
+        this.setState(
+          {
+            isLoading: false,
+            dataSource: responseJson.movies,
+          },
+          function() {},
+        );
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+
   render() {
+    if (this.state.isLoading) {
+      return (
+        <View style={{flex: 1, padding: 20}}>
+          <ActivityIndicator />
+        </View>
+      );
+    }
+
     return (
-      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        <Text>Hello, world!</Text>
+      <View style={{flex: 1, paddingTop: 20}}>
+        <FlatList
+          data={this.state.dataSource}
+          renderItem={({item}) => (
+            <Text>
+              {item.title}, {item.releaseYear}
+            </Text>
+          )}
+          keyExtractor={({id}, _) => id}
+        />
       </View>
     );
   }
